d1c4943e6c2fd68ad351089be20751787e3ff021
vkirda <vkirda@yahoo.com>
2018-06-30 16:26:52 +0300

    javascript for 2 pages done

--- assets/js/tesonetjs.js
+++ assets/js/tesonetjs.js
@@ -1,18 +1,61 @@
 $(function(){
 
-	//console.log("veikia");
+	var homeLinkHeader = document.querySelector('.home-link-header');
+	var featuresLinkHeader = document.querySelector('.features-link-header');
+	var reviewsLinkHeader = document.querySelector('.reviews-link-header');
+	var downloadLinkHeader = document.querySelector('.download-link-header');
+
+	var homeLinkFooter = document.querySelector('.home-link-footer');
+	var featuresLinkFooter = document.querySelector('.features-link-footer');
+	var reviewsLinkFooter = document.querySelector('.reviews-link-footer');
+	var downloadLinkFooter = document.querySelector('.download-link-footer');
+
+	var downloadButton = document.querySelector('.description__buttons a:first-child button');
+	var featuresButton = document.querySelector('.description__buttons a:last-child button');
+//console.log(downloadButton);
+//console.log(featuresButton);
+	var reviewsSection = document.querySelector('.reviews-section');
+	var fearuresSection = document.querySelector('.features');
+	var downloadSection = document.querySelector('.download');
+
+	reviewsSection.classList.add('hidden-section');
+
 
 	$.getJSON('assets/js/reviews-section.json', function(data) {
-		//console.log(data);
-	    //var template = $('#personTpl').html();
 	    var template = $('.reviews-script').html();
-
-	    //console.log(template);
-	    //var html = Mustache.render(template, data);
 	    var html = Mustache.render(template, data);
-	    //console.log(html);
-	    //$('#sampleArea').html(html);
 	    $('.reviews-section').html(html);
 	});
+
+	homeLinkHeader.addEventListener('click', goFrontPage);
+	featuresLinkHeader.addEventListener('click', goFrontPage);
+	downloadLinkHeader.addEventListener('click', goFrontPage);
+
+	homeLinkFooter.addEventListener('click', goFrontPage);
+	featuresLinkFooter.addEventListener('click', goFrontPage);
+	downloadLinkFooter.addEventListener('click', goFrontPage);
+
+	downloadButton.addEventListener('click', goFrontPage);
+	featuresButton.addEventListener('click', goFrontPage);
+
+	reviewsLinkHeader.addEventListener('click', goSecondPage);
+	reviewsLinkFooter.addEventListener('click', goSecondPage);
+
+	function goFrontPage(){
+		console.log('veikia font');
+
+		fearuresSection.classList.remove('hidden-section');
+		downloadSection.classList.remove('hidden-section');
+		reviewsSection.classList.add('hidden-section');
+	}
+
+	function goSecondPage(){
+
+		fearuresSection.classList.add('hidden-section');
+		downloadSection.classList.add('hidden-section');
+		reviewsSection.classList.remove('hidden-section');
+		console.log('clickas veikia second');
+		
+	}
 
 });
